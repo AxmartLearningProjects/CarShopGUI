4e424c732b592ea610ff0d4c7e4b9ce1e540ff8e
Axmart <notaxmart@gmail.com>
2020-09-05 18:47:33 +0200

    Create car function

--- CarShopGUI/Form1.cs
+++ CarShopGUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,11 @@ namespace CarShopGUI
 {
     public partial class Form1 : Form
     {
+
+        Store myStore = new Store();
+        BindingSource carInventoryBindingSource = new BindingSource();
+        BindingSource cartBindingSource = new BindingSource();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +25,16 @@ namespace CarShopGUI
 
         private void btn_create_car_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("create car");
+
+            Car c = new Car(txt_make.Text, txt_model.Text, decimal.Parse(txt_price.Text));
+            //MessageBox.Show(c.ToString());
+            myStore.CarList.Add(c);
+            carInventoryBindingSource.ResetBindings(false);
+            txt_make.Text = String.Empty;
+            txt_model.Text = String.Empty;
+            txt_price.Text = String.Empty;
+
+
         }
 
         private void btn_add_to_cart_Click(object sender, EventArgs e)
@@ -29,6 +44,15 @@ namespace CarShopGUI
 
         private void btn_checkout_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carInventoryBindingSource.DataSource = myStore.CarList;
+            lst_inventory.DataSource = carInventoryBindingSource;
+            lst_inventory.DisplayMember = ToString();
+
 
         }
     }
